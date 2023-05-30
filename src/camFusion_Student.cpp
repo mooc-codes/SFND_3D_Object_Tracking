24c9e2c7455d3767488cda0349a9deff7fc642ea
mooc-codes <75176247+mooc-codes@users.noreply.github.com>
2023-05-30 06:09:16 +0000

    Fix ttc sign

--- src/camFusion_Student.cpp
+++ src/camFusion_Student.cpp
@@ -189,7 +189,7 @@ void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
     s1 = abs(minCurr1 - minCurr2) < thresh ? minCurr1 : minCurr2;
     dt = 1 / frameRate; 
 
-    TTC =  (s1 * dt) / (s1 - s0);  
+    TTC =  (s1 * dt) / (s0 - s1);  
 
 }
 
