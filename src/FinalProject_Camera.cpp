4e8dd2df315aa480653d215cf2c2251e251d2cbe
mooc-codes <75176247+mooc-codes@users.noreply.github.com>
2023-05-31 11:54:04 +0000

    FAST + BRISK

--- src/FinalProject_Camera.cpp
+++ src/FinalProject_Camera.cpp
@@ -188,7 +188,7 @@ int main(int argc, const char *argv[])
         /* EXTRACT KEYPOINT DESCRIPTORS */
 
         cv::Mat descriptors;
-        string descriptorType = "BRIEF"; // BRISK, BRIEF, ORB, FREAK, AKAZE, SIFT
+        string descriptorType = "BRISK"; // BRISK, BRIEF, ORB, FREAK, AKAZE, SIFT
         descKeypoints((dataBuffer.end() - 1)->keypoints, (dataBuffer.end() - 1)->cameraImg, descriptors, descriptorType);
 
         // push descriptors for current frame to end of data buffer
